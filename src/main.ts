2bd56069395226d69893bdb9fad0604921541bc4
emre1n <emre1n@protonmail.com>
2025-08-13 21:31:30 +0300

    setup and register counter reducer

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideStore } from '@ngrx/store';
+
+import { counterReducer } from './app/store/counter.reducer';
 
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideStore({
+      counter: counterReducer,
+    }),
+  ],
+});
