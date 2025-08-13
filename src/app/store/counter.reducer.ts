b4fd2507a61f8e3b71f9ea861f531ef557cfa395
emre1n <emre1n@protonmail.com>
2025-08-13 21:54:16 +0300

    add increment action and implement it in counter controls component

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,11 +1,15 @@
-import { createReducer } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
+import { increment } from './counter.actions';
 
 const initialState = 0;
 
 // createReducer is a function that creates a reducer function (New way)
-// export const counterReducer = createReducer(initialState);
+export const counterReducer = createReducer(
+  initialState,
+  on(increment, (state) => state + 1)
+);
 
 // This is a function that creates a reducer function (Old way)
-export function counterReducer(state = initialState) {
-  return state;
-}
+// export function counterReducer(state = initialState) {
+//   return state;
+// }
