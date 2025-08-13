8827975bcd04cd73195e9d5c0e942608e63bef72
emre1n <emre1n@protonmail.com>
2025-08-13 22:38:36 +0300

    add counter selectors

--- src/app/counter-output/counter-output.component.ts
+++ src/app/counter-output/counter-output.component.ts
@@ -3,6 +3,8 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 
+import { selectCount, selectDoubleCount } from '../store/counter.selectors';
+
 @Component({
   selector: 'app-counter-output',
   templateUrl: './counter-output.component.html',
@@ -12,8 +14,10 @@ import { AsyncPipe } from '@angular/common';
 })
 export class CounterOutputComponent {
   count$: Observable<number>;
+  doubleCount$: Observable<number>;
 
   constructor(private store: Store<{ counter: number }>) {
-    this.count$ = store.select('counter');
+    this.count$ = store.select(selectCount);
+    this.doubleCount$ = store.select(selectDoubleCount);
   }
 }
