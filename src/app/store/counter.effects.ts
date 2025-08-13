c6689ce86ab5855cc996912bbbafa4a0c00685da
emre1n <emre1n@protonmail.com>
2025-08-13 22:52:21 +0300

    add counter effects

---
+++ src/app/store/counter.effects.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { tap, withLatestFrom } from 'rxjs';
+
+import { decrement, increment } from './counter.actions';
+import { selectCount } from './counter.selectors';
+
+@Injectable()
+export class CounterEffects {
+  saveCount = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(increment, decrement),
+        withLatestFrom(this.store.select(selectCount)),
+        tap(([action, counter]) => {
+          console.log(action);
+          localStorage.setItem('count', counter.toString());
+        })
+      ),
+    {
+      dispatch: false,
+    }
+  );
+
+  constructor(
+    private actions$: Actions,
+    private store: Store<{ counter: number }>
+  ) {}
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 import { counterReducer } from './app/store/counter.reducer';
+import { CounterEffects } from './app/store/counter.effects';
 
 import { AppComponent } from './app/app.component';
 
@@ -10,5 +12,6 @@ bootstrapApplication(AppComponent, {
     provideStore({
       counter: counterReducer,
     }),
+    provideEffects([CounterEffects]),
   ],
 });
